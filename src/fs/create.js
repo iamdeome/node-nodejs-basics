bcaa54f6b368d86052b04fdeaba763f949890196
krak <alexedtrifonov@gmail.com>
2024-01-26 16:41:33 +0100

    feat: add create.js sollution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { access, writeFile, constants } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+const file = 'fresh.txt';
+const dir = 'files';
+const filePath = path.join(path.dirname(fileURLToPath(import.meta.url)), dir, file);
+
 const create = async () => {
-    // Write your code here 
+
+    try {
+        await access(filePath, constants.R_OK);
+        console.error(`FS operation failed`);
+    } catch {
+        await writeFile(filePath, 'I am fresh and young');
+        console.log(`File '${file}' created successfully in the 'files' folder.`);
+    } 
 };
 
 await create();
