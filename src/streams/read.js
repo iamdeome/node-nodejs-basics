4ba5be27e3ec647c21e16f60c4625d622b747610
krak <alexedtrifonov@gmail.com>
2024-01-27 18:57:21 +0100

    feat: add stream:read sollution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+
+const dir = 'files';
+const file = 'fileToRead.txt';
+const filePath = path.join(path.dirname(fileURLToPath(import.meta.url)), dir, file);
+
 const read = async () => {
-    // Write your code here 
+    const fileStream = createReadStream(filePath, { encoding: 'utf-8' });
+    fileStream.pipe(process.stdout);
+
+    fileStream.on('end', () => {
+        console.log('\n');
+      });
+
+    fileStream.on('error', (err) => {
+        console.error('FS operation failed', err);
+    });
+
 };
 
 await read();
