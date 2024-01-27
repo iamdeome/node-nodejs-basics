71bdc2cd46ace95a969e44399b16e3e51bb79d3a
krak <alexedtrifonov@gmail.com>
2024-01-27 19:07:36 +0100

    feat: add stream:write sollution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+
+const dir = 'files';
+const file = 'fileToWrite.txt';
+const filePath = path.join(path.dirname(fileURLToPath(import.meta.url)), dir, file);
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(filePath, {encoding: 'utf-8'});
+
+    process.stdin.pipe(stream);
+
+    stream.on('finish', () => {
+        console.log("done");
+    })
+
+    stream.on('error', (err) => {
+        console.error('FS operation failed');
+    });
 };
 
 await write();
