fe05debfa2aece2c6cd735b0cfe5e524d51ccb20
krak <alexedtrifonov@gmail.com>
2024-01-29 14:06:21 +0100

    feat implement transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,12 @@
+import { Transform  } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const revers = new Transform ({
+        transform(c, e, cb) { 
+            cb(null, c.toString().split('').reverse().join(''));
+        }
+    })
+    process.stdin.pipe(revers).pipe(process.stdout);
 };
 
 await transform();
