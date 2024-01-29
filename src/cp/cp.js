4a4cd62130cad53668a88eb401be13d0b58afd91
krak <alexedtrifonov@gmail.com>
2024-01-29 17:39:03 +0100

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import { spawn } from 'child_process';
+import { stdin, stdout } from 'process';
+
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+
+const dir = 'files';
+const file = 'script.js';
+const filePath = path.join(path.dirname(fileURLToPath(import.meta.url)), dir, file);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn('node', [filePath, ...args], { stdio: 'pipe' });
+
+  stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(stdout); 
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', '3']);
